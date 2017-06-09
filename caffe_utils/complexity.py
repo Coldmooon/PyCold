87802160cef185f29d8456155fb5e88f53f1bb19
coldmooon <liy@bupt.edu.cn>
2017-06-09 21:01:55 +0800

    Skip some keywords in complexity.py

--- caffe_utils/complexity.py
+++ caffe_utils/complexity.py
@@ -8,4 +8,9 @@ def params_network(prototxt_file):
 
 def params_solver(caffe_solver):
     print('Number of parameters:')
-    print(np.sum([np.prod(v[0].data.shape) for k, v in caffe_solver.net.params.items() if 'bn' not in k]))
+    print(np.sum([np.prod(v[0].data.shape) for k, v in caffe_solver.net.params.items() if 'conv' in k if 'bn' not in k if 'shared' not in k]))
+    print('shared parameters:')
+    for k,v in caffe_solver.net.params.items():
+        if 'shared' in  k:
+            print(np.prod(v[0].data.shape))
+            break
