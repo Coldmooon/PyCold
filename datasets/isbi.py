43226201588a74e1f20e53e031204d2ce636de1b
coldmooon <liy@bupt.edu.cn>
2017-09-07 15:18:50 +0800

    Updated isbi dataset script

--- datasets/isbi.py
+++ datasets/isbi.py
@@ -30,17 +30,29 @@ labels = Image.open(train_labels_file)
 #     imgs.seek(imgs.tell()+1)
 #     labels.seek(labels.tell() + 1)
 
+use_pad = 1
+pad = 23
 
 canvas = [47, 47]
 size = int(imgs.size[0]/2)
-dim_x = size - canvas[0] + 1
-dim_y = size - canvas[1] + 1
+train_size = int(imgs.size[0]/2)
+if use_pad:
+    train_padded_size = train_size + pad*2
+    padded_size = size + pad*2
+else:
+    train_padded_size = train_size
+    padded_size = size
 
-train_dataset = np.zeros((dim_x * dim_y * 25, 1, canvas[0], canvas[1]), dtype=np.uint8)
-test_dataset  = np.zeros((dim_x * dim_y * 5,  1, canvas[0], canvas[1]), dtype=np.uint8)
+train_dim_x = train_padded_size - canvas[0] + 1
+train_dim_y = train_padded_size - canvas[1] + 1
+test_dim_x = padded_size - canvas[0] + 1
+test_dim_y = padded_size - canvas[1] + 1
 
-train_labels = np.zeros((dim_x * dim_y * 25), dtype=np.uint8)
-test_labels =  np.zeros((dim_x * dim_y * 5), dtype=np.uint8)
+train_dataset = np.zeros((train_dim_x * train_dim_y * 25, 1, canvas[0], canvas[1]), dtype=np.uint8)
+test_dataset  = np.zeros((test_dim_x * test_dim_y * 5,  1, canvas[0], canvas[1]), dtype=np.uint8)
+
+train_labels = np.zeros((train_dim_x * train_dim_y * 25), dtype=np.uint8)
+test_labels =  np.zeros((test_dim_x * test_dim_y * 5), dtype=np.uint8)
 
 stride = 1
 train_count = 0
@@ -49,13 +61,24 @@ for frame in ImageSequence(imgs):
     idx = frame.tell()
     labels.seek(idx)
     print('processing the', idx, 'th image ... and ', labels.tell(), 'th label')
-    im = np.array(frame.resize((size, size)))
-    # zero_padded_im = np.pad(im, pad_width=((23, 23), (23, 23)), mode='constant', constant_values=0)
-    label = np.array(labels.resize((size, size)))
+    if idx < 25:
+        im = np.array(frame.resize((train_size, train_size)))
+        label = np.array(labels.resize((train_size, train_size)))
+        dx = train_dim_x
+        dy = train_dim_y
+    else:
+        im = np.array(frame.resize((size, size)))
+        label = np.array(labels.resize((size, size)))
+        dx = test_dim_x
+        dy = test_dim_y
 
-    for x in range(dim_x):
+    if use_pad:
+        im = np.pad(im, pad_width=((pad, pad), (pad, pad)), mode='reflect')
+        label = np.pad(label, pad_width=((pad, pad), (pad, pad)), mode='reflect')
+
+    for x in range(dx):
         # print('coor: x: [', x, ':', x + canvas[0], '];')
-        for y in range(dim_y):
+        for y in range(dy):
             # print('y: [', y, ':', y + canvas[1], '].')
             patch = im[x : x + canvas[0], y : y + canvas[1]]
             # padded_patch = zero_padded_im[x : x + canvas[0], y : y + canvas[1]]
@@ -74,8 +97,8 @@ for frame in ImageSequence(imgs):
     print('cut out ', train_count, 'training samples and', test_count, 'test samples ...')
 
 
-num_white = 200000
-num_black = 100000
+num_white = 366547
+num_black = 366547
 
 # data balance
 n_train_class_white = np.sum(train_labels == 255)
