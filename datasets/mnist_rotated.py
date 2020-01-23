b35b2e732eeb76062f22dff49e7ecf13f3b64b5f
Coldmooon <mingyang@tsinghua.edu.cn>
2020-01-23 10:02:38 +0000

    modified mnist rotated

--- datasets/mnist_rotated.py
+++ datasets/mnist_rotated.py
@@ -4,7 +4,7 @@ import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 from torch.autograd import Variable
 import numpy as np
-from datasets.LMDB.caffedb import *
+# from datasets.LMDB.caffedb import *
 import matplotlib.pyplot as plt
 from scipy.ndimage import rotate
 from skimage.transform import warp
@@ -24,25 +24,25 @@ test_dataset = dsets.MNIST(root='./data/',
 canvas = [42, 42]
 
 train_data = train_dataset.train_data.numpy()
-train_data_transformed = np.zeros((train_data.shape[0], canvas[0], canvas[1]))
-train_data_center_only = np.zeros((train_data.shape[0], canvas[0], canvas[1]))
-train_data_rand_placed = np.zeros((train_data.shape[0], canvas[0], canvas[1]))
+train_data_transformed = np.zeros((train_data.shape[0], canvas[0], canvas[1]), dtype=np.uint8)
+train_data_center_only = np.zeros((train_data.shape[0], canvas[0], canvas[1]), dtype=np.uint8)
+train_data_rand_placed = np.zeros((train_data.shape[0], canvas[0], canvas[1]), dtype=np.uint8)
 # randomly transform the training data
 print('Transforming the training data ...')
 for i in range(train_data.shape[0]):
 
     img = train_data[i]
 
-    plt.figure(figsize=(7, 7))
-    plt.imshow(img.reshape(28, 28), cmap='gray', interpolation='none')
-    plt.title('Original MNIST', fontsize=20)
-    plt.axis('off')
-    plt.show()
+    # plt.figure(figsize=(7, 7))
+    # plt.imshow(img.reshape(28, 28), cmap='gray', interpolation='none')
+    # plt.title('Original MNIST', fontsize=20)
+    # plt.axis('off')
+    # plt.show()
 
     rotation = np.random.uniform(-np.pi/4, np.pi/4) # * 180 / np.pi
     # scale = np.random.uniform(0.7, 1.2)
-    scale = np.random.uniform(0.7, 10/3)
-    # scale = np.random.uniform(0.83, 1.42) # corresponding to STN (1/0.7, 1/1.2)
+    # scale = np.random.uniform(0.7, 1.2)
+    scale = np.random.uniform(0.83, 1.42) # corresponding to STN (1/0.7, 1/1.2)
 
     R = np.array([[np.cos(rotation), -np.sin(rotation), 0],
                   [np.sin(rotation),  np.cos(rotation), 0],
@@ -53,29 +53,29 @@ for i in range(train_data.shape[0]):
 
     T = (np.dot(S, R))[0:2,:]
 
-    im = affine_transformer(img[None, :,:, None], S)
+    im = affine_transformer(img[None, :,:, None], T)
     # train_data[i,:,:] = rotate(train_data[i], rotation, reshape=False)
+    #
+    # plt.figure(figsize=(7, 7))
+    # plt.imshow(im[0,:,:,0].reshape(28, 28), cmap='gray', interpolation='none')
+    # plt.title('Transformed MNIST', fontsize=20)
+    # plt.axis('off')
+    # plt.show()
 
-    plt.figure(figsize=(7, 7))
-    plt.imshow(im[0,:,:,0].reshape(28, 28), cmap='gray', interpolation='none')
-    plt.title('Transformed MNIST', fontsize=20)
-    plt.axis('off')
-    plt.show()
-
-    placed_h_random = np.random.randint(0 - 0.1, canvas[0] - 28)
-    placed_w_random = np.random.randint(0 - 0.1, canvas[1] - 28)
-    placed_h_center = (canvas[0] - 28) / 2
-    placed_w_center = (canvas[1] - 28) / 2
+    placed_h_random = np.random.randint(-1 + 0.1, canvas[0] - 28 + 1)
+    placed_w_random = np.random.randint(-1 + 0.1, canvas[1] - 28 + 1)
+    placed_h_center = int((canvas[0] - 28) / 2)
+    placed_w_center = int((canvas[1] - 28) / 2)
 
     train_data_transformed[i, placed_h_random:placed_h_random + 28, placed_w_random:placed_w_random + 28] = im[0,:,:,0]
     train_data_center_only[i, placed_h_center:placed_h_center + 28, placed_w_center:placed_w_center + 28] = img[:,:]
     train_data_rand_placed[i, placed_h_random:placed_h_random + 28, placed_w_random:placed_w_random + 28] = img[:,:]
-
-    plt.figure(figsize=(7, 7))
-    plt.imshow(train_data_center_only[i].reshape(canvas[0], canvas[1]), cmap='gray', interpolation='none')
-    plt.title('no scale', fontsize=20)
-    plt.axis('off')
-    plt.show()
+    #
+    # plt.figure(figsize=(7, 7))
+    # plt.imshow(train_data_transformed[i].reshape(canvas[0], canvas[1]), cmap='gray', interpolation='none')
+    # plt.title('center only', fontsize=20)
+    # plt.axis('off')
+    # plt.show()
 
 train_data_transformed = np.expand_dims(train_data_transformed, axis=1)
 train_data_center_only = np.expand_dims(train_data_center_only, axis=1)
@@ -84,7 +84,7 @@ train_labels = train_dataset.train_labels.numpy()
 
 print('Transforming the test data ...')
 test_data = test_dataset.test_data.numpy()
-test_data_transformed = np.zeros((test_data.shape[0], canvas[0], canvas[1]))
+test_data_transformed = np.zeros((test_data.shape[0], canvas[0], canvas[1]), dtype=np.uint8)
 for i in range(test_data.shape[0]):
 
     img = test_data[i]
@@ -96,9 +96,9 @@ for i in range(test_data.shape[0]):
     # plt.show()
 
     rotation = np.random.uniform(-np.pi/4, np.pi/4) # * 180 / np.pi
+
     # scale = np.random.uniform(0.7, 1.2)
-    scale = np.random.uniform(0.7, 10/3)
-    # scale = np.random.uniform(0.83, 1.42) # corresponding to STN (1/0.7, 1/1.2)
+    scale = np.random.uniform(0.83, 1.42) # corresponding to STN (1/0.7, 1/1.2)
 
     R = np.array([[np.cos(rotation), -np.sin(rotation), 0],
                   [np.sin(rotation),  np.cos(rotation), 0],
@@ -108,18 +108,18 @@ for i in range(test_data.shape[0]):
                   [0,   0,    1]])
 
     T = (np.dot(S, R))[0:2,:]
-    im = affine_transformer(img[None, :,:, None], S)
+    im = affine_transformer(img[None, :,:, None], T)
 
     # test_data[i,:,:] = rotate(test_data[i], rotation, reshape=False)
-
+    #
     # plt.figure(figsize=(7, 7))
     # plt.imshow(im.reshape(28, 28), cmap='gray', interpolation='none')
     # plt.title('Transformed MNIST', fontsize=20)
     # plt.axis('off')
     # plt.show()
 
-    placed_h = np.random.randint(0 - 0.1, canvas[0] - 28)
-    placed_w = np.random.randint(0 - 0.1, canvas[1] - 28)
+    placed_h = np.random.randint(-1 + 0.1, canvas[0] - 28 + 1)
+    placed_w = np.random.randint(-1 + 0.1, canvas[1] - 28 + 1)
 
     test_data_transformed[i,placed_h:placed_h + 28, placed_w:placed_w + 28] = im[0,:,:,0]
 
@@ -141,10 +141,10 @@ print('test_data_transformed shape: ', test_data_transformed.shape)
 
 # dataset = {'train': [train_data_transformed, train_labels], 'test': [test_data_transformed, test_labels]}
 # for caffe
-numpy2lmdb([train_data_transformed, train_labels], 'train_transformed')
-numpy2lmdb([train_data_center_only, train_labels], 'train_data_center_only')
-numpy2lmdb([train_data_rand_placed, train_labels], 'train_rand_placed')
-numpy2lmdb([test_data_transformed, test_labels], 'test_transformed')
+# numpy2lmdb([train_data_transformed, train_labels], 'train_transformed')
+# numpy2lmdb([train_data_center_only, train_labels], 'train_data_center_only')
+# numpy2lmdb([train_data_rand_placed, train_labels], 'train_rand_placed')
+# numpy2lmdb([test_data_transformed, test_labels], 'test_transformed')
 
 # for torch
 np.save('train_transformed.npy', train_data_transformed)
