8125f4bc31953acecbae2ba231660a6904a1a122
Coldmooon <mingyang@tsinghua.edu.cn>
2020-01-28 09:48:27 +0000

    fixed a bug of making symbolic link

--- datasets/imagenet/training_data.sh
+++ datasets/imagenet/training_data.sh
@@ -1,7 +1,7 @@
 
 
 # https://github.com/facebookarchive/fb.resnet.torch/blob/master/INSTALL.md#download-the-imagenet-dataset
-mkdir train && mv ILSVRC2012_img_train.tar train/ && cd train
+mkdir train && ln -s ../ILSVRC2012_img_train.tar train/ILSVRC2012_img_train.tar && cd train
 tar -xvf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar
 find . -name "*.tar" | while read NAME ; do mkdir -p "${NAME%.tar}"; tar -xvf "${NAME}" -C "${NAME%.tar}"; rm -f "${NAME}"; done
 cd ..
--- datasets/imagenet/val_data.sh
+++ datasets/imagenet/val_data.sh
@@ -1,3 +1,3 @@
 # https://github.com/facebookarchive/fb.resnet.torch/blob/master/INSTALL.md#download-the-imagenet-dataset
-mkdir val && mv ILSVRC2012_img_val.tar val/ && cd val && tar -xvf ILSVRC2012_img_val.tar
+mkdir val && ln -s ../ILSVRC2012_img_val.tar val/ILSVRC2012_img_val.tar && cd val && tar -xvf ILSVRC2012_img_val.tar
 wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
